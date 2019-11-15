e327acac29012b3967455df9136892a25715f916
Rohan <rhnmht30@gmail.com>
2019-11-16 00:19:12 +0530

    init

---
+++ generateClassifier.py
@@ -0,0 +1,37 @@
+# Import the modules
+from sklearn.externals import joblib
+from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+# from sklearn.neighbors import KNeighborsClassifier
+# from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from collections import Counter
+
+# Load the dataset
+dataset = datasets.fetch_openml("mnist_784")
+
+# Extract the features and labels
+features = np.array(dataset.data, 'int16')
+labels = np.array(dataset.target, 'int')
+
+# Extract the hog features
+list_hog_fd = []
+for feature in features:
+    fd = hog(feature.reshape((28, 28)), orientations=9, pixels_per_cell=(
+        14, 14), cells_per_block=(1, 1), visualize=False)
+    list_hog_fd.append(fd)
+hog_features = np.array(list_hog_fd, 'float64')
+
+print("Count of digits in dataset", Counter(labels))
+
+# Create an linear SVM object
+clf = LinearSVC()
+# clf = KNeighborsClassifier()
+# clf = RandomForestClassifier(n_estimators=100)
+
+# Perform the training
+clf.fit(hog_features, labels)
+
+# Save the classifier
+joblib.dump(clf, "digits_cls.pkl", compress=0)
